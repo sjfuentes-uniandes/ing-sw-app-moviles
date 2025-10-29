66420b1c5c7ef67b5169cf03bdc87fbe71dc7c1b
capb90 <capb.dev@gmail.com>
2025-10-29 14:02:25 -0500

    Fix: Layout Manager fix

--- app/src/main/java/com/example/vinilos/ui/artists/ArtistsFragment.kt
+++ app/src/main/java/com/example/vinilos/ui/artists/ArtistsFragment.kt
@@ -8,7 +8,7 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.vinilos.R
 import com.example.vinilos.databinding.FragmentArtistsBinding
@@ -37,7 +37,7 @@ class ArtistsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         recyclerView = binding.artistsRecyclerView
-        recyclerView.layoutManager = GridLayoutManager(context, 2)
+        recyclerView.layoutManager = LinearLayoutManager(context)
         recyclerView.adapter = artistsAdapter
 
         binding.swipeRefresh.setOnRefreshListener {
