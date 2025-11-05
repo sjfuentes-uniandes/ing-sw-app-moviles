48ca5ea1fae2acd255a9994bfb40896142dde81c
Santiago Fuentes <sj.fuentes@uniandes.edu.co>
2025-11-04 22:39:31 -0500

    mejorar el uso de glide para el cacheo de imagenes

--- app/src/main/java/com/example/vinilos/ui/adapters/CollectorsAdapter.kt
+++ app/src/main/java/com/example/vinilos/ui/adapters/CollectorsAdapter.kt
@@ -2,9 +2,11 @@ package com.example.vinilos.ui.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.net.toUri
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.vinilos.R
 import com.example.vinilos.databinding.CollectorItemBinding
 import com.example.vinilos.models.Collector
@@ -30,22 +32,24 @@ class CollectorsAdapter: RecyclerView.Adapter<CollectorsAdapter.CollectorViewHol
     }
 
     override fun onBindViewHolder(holder: CollectorViewHolder, position: Int) {
+        holder.viewDataBinding.also {
+            it.collector = collectors[position]
+        }
         holder.bind(collectors[position])
     }
 
     override fun getItemCount(): Int = collectors.size
 
-    class CollectorViewHolder(val binding: CollectorItemBinding): RecyclerView.ViewHolder(binding.root){
+    class CollectorViewHolder(val viewDataBinding: CollectorItemBinding): RecyclerView.ViewHolder(viewDataBinding.root){
         fun bind(collector: Collector){
-            binding.collector = collector
-
-            Glide.with(binding.root.context)
-                .load("")
-                .placeholder(R.drawable.placeholder_image)
-                .error(R.drawable.placeholder_image)
-                .into(binding.collectorImage)
-
-            binding.executePendingBindings()
+            Glide.with(itemView)
+                .load("".toUri().buildUpon().scheme("https").build())
+                .apply(
+                    RequestOptions()
+                        .placeholder(R.drawable.placeholder_image)
+                        .error(R.drawable.placeholder_image)
+                )
+                .into(viewDataBinding.collectorImage)
         }
     }
 }
